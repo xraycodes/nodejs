549ab22c56604287c8f61c71f9b68b6148aff2f2
brianjo94 <brianjo1994@gmail.com>
2023-12-12 20:05:04 -0500

    learnig about fs.. native node modules

---
+++ reference/fs_demo.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const path = require('path');
+
+// Create folder
+fs.mkdir(path.join(__dirname, '/test'), {}, err => {
+    if(err) throw err;
+    console.log('folder created...');
+})
+
+// Create and write to file
+fs.writeFile(path.join(__dirname, '/test','hello.txt'), 'Hello World!', err => {
+    if(err) throw err;
+    console.log('File written to...');
+})
